c5a69e067038db0eb07df8b610e538df7f1f7cbd
cyicea <cyicea@163.com>
2019-06-19 23:24:21 +0800

    加了个空格

--- src/packages/packages.go
+++ src/packages/packages.go
@@ -9,5 +9,5 @@ func main() {
 	fmt.Printf("My favorite number is %d.", rand.Intn(10))
 	fmt.Println()
 	fmt.Println("My favorite number is", rand.Intn(10))
-	fmt.Printf("My favorite number is %d.", rand.Intn(10))
+	fmt.Printf("My favorite number is %d. ", rand.Intn(10))
 }
