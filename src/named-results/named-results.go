d3d6514d622157b9b26057334c8b4dc37e3aac0b
cyicea <cyicea@163.com>
2019-06-19 23:12:27 +0800

    家中主机提交

--- src/named-results/named-results.go
+++ src/named-results/named-results.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func split(sum int) (x, y int) {
+func split(sum float32) (x, y float32) {
 	x = sum * 4 / 9
 	y = sum - x
 	return
 }
 
 func main() {
-	fmt.Println(split(171))
+	fmt.Println(split(17))
 }
